b0b1cd0245e9b43e0919a21438d37a3af3c3e061
gustasousagh <158321110+gustasousagh@users.noreply.github.com>
2024-10-07 14:54:23 -0300

    add some modifications

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -57,51 +57,57 @@ const RoomPlayersList = () => {
   if (isLoading) {
     return <div>Carregando...</div>;
   }
-  const numbers = [0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 22, 89];
+  const numbers = [0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 22, 24];
   return (
     <div className="flex flex-col h-full justify-center items-center">
-      <ul className="flex overflow-y-auto flex-row">
-        {players.map((player) => (
-          <PlayerCard key={player._id} hasChosenNumber={player.hasChosenNumber} name={player.user.name} selected={typeof player.selectedNumber === "number" ? player.selectedNumber : undefined}
-          image={player.user.image}/>
-        ))}
-      </ul>
+  <ul className="flex overflow-y-auto flex-row max-h-96 w-full justify-center">
+    {players.map((player) => (
+      <PlayerCard 
+        key={player._id} 
+        hasChosenNumber={player.hasChosenNumber} 
+        name={player.user.name} 
+        selected={typeof player.selectedNumber === "number" ? player.selectedNumber : undefined}
+        image={player.user.image}
+      />
+    ))}
+  </ul>
 
-      <div className="overflow-x-hidden whitespace-nowrap p-4">
-        <div className="flex gap-2">
-          {numbers.map((number) => (
-             <button
-             key={number}
-             onClick={() => handleNumberClick(number)}
-             className={`p-2 w-12 h-12 border border-gray-300 rounded ${
-               selectedNumber === number ? "bg-blue-500 text-white" : "bg-white text-gray-800"
-             }`}
-             disabled={isSelectingNumber}
-           >
-             {number}
-           </button>
-       
-          ))}
-        </div>
-      </div>
-
-      <div className="flex gap-4 mt-4">
-        <Button 
-          onClick={handleRevealClick} 
-          disabled={isRevealingNumbers} 
-          className="bg-green-500 text-white rounded"
+  <div className="overflow-x-auto whitespace-nowrap p-4 max-w-full flex justify-center">
+    <div className="flex gap-2">
+      {numbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => handleNumberClick(number)}
+          className={`p-2 w-12 h-12 border border-gray-300 rounded ${
+            selectedNumber === number ? "bg-blue-500 text-white" : "bg-white text-gray-800"
+          }`}
+          disabled={isSelectingNumber}
         >
-          Revelar Números
-        </Button>
-        <Button 
-          onClick={handleResetClick} 
-          disabled={isResetting} 
-          className="bg-red-500 text-white rounded"
-        >
-          Resetar Visibilidade
-        </Button>
-      </div>
+          {number}
+        </button>
+      ))}
     </div>
+  </div>
+
+  <div className="flex gap-4 mt-4 justify-center">
+    <Button 
+      onClick={handleRevealClick} 
+      disabled={isRevealingNumbers} 
+      className="bg-green-500 text-white rounded"
+    >
+      Revelar Números
+    </Button>
+    <Button 
+      onClick={handleResetClick} 
+      disabled={isResetting} 
+      className="bg-red-500 text-white rounded"
+    >
+      Resetar Visibilidade
+    </Button>
+  </div>
+</div>
+
+
   );
 };
 
